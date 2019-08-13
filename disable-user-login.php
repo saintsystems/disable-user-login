c2bf0ffb57145fa3377cebdcebf93530ceeec58f
Adam Anderly <adam@anderly.com>
2019-08-13 16:00:26 -0500

    Update disable-user-login.php

--- disable-user-login.php
+++ disable-user-login.php
@@ -7,28 +7,11 @@
  * Author:      Saint Systems
  * Author URI:  https://www.saintsystems.com
  * Text Domain: disable-user-login
- * License:     GNU General Public License v3.0
+ * Domain Path: languages
+ *
+ * Copyright: � 2019 Saint Systems
+ * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * Domain Path: /languages
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * @author    Saint Systems
- * @version   1.0.0
- * @package   Disable User Login
- * @license   GPL3
- * @author    Saint Systems
- * @link      http://www.saintsystems.com
- * @copyright Copyright 2019, Saint Systems, LLC
- *
  */
 
 /** If this file is called directly, abort. */
